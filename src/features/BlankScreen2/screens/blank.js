68522609a6ba7eeee04a396a18f812d49cf98677
Crowdbotics <team@crowdbotics.com>
2020-04-21 19:26:57 +0000

    Update _Blank JSX

--- src/features/BlankScreen2/screens/blank.js
+++ src/features/BlankScreen2/screens/blank.js
@@ -1,19 +1,17 @@
-import React from "react";
-import { View } from "react-native";
-import {
-  withStyles,
-} from 'react-native-ui-kitten';
+import React from "react"
+import { View } from "react-native"
+import { withStyles } from "react-native-ui-kitten"
 
 export class _Blank extends React.Component {
   static navigationOptions = {
     title: "Blank".toUpperCase()
-  };
+  }
 
-  render = () => <View style={this.props.themedStyle.container}></View>;
+  render = () => <View style={this.props.themedStyle.container}></View>
 }
 
 export default Blank = withStyles(_Blank, theme => ({
   container: {
     backgroundColor: theme["color-basic-100"]
   }
-}));
+}))
